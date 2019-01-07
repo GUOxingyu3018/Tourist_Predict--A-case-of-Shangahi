7108d2b853b4c5051bf2402b3d7ce9619878c585
星宇 郭 <gxydewr@hotmail.com>
2019-01-07 20:13:04 +0800

    确定将采用的时间序列预测方法

---
+++ Tourist Predict--A case of Shanghai/ARIMA.py
@@ -0,0 +1,63 @@
+from statsmodels import * 
+import numpy as np
+import pandas as pd
+import itertools
+from matplotlib import *
+import statsmodels.api as sm
+
+data = pd.read_excel(r'Data/')#读取excel数据
+
+
+#数据平稳性检验，确定模型的D取值
+def stationarity_Test(data):
+    data_diff = data.diff()
+    pyplot.plot(data, label = '原始数据', colors = 'black')
+    pyplot.plot(data_diff, label = '一阶差分', colors = 'red')
+    pyplot.legend()
+    pyplot.show()
+
+
+#确定模型的P,Q最佳取值
+def decide_PQ():   
+    p = d = q = range(0, 4)    
+    pdq = list(itertools.product(p, d, q))   
+    seasonal_pdq = [(x[0], x[1], x[2], 12) for x in pdq]
+    
+    print('Examples of parameter combinations for Seasonal ARIMA...')
+    print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[1]))
+    print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[2]))
+    print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[3]))
+    print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[4]))
+
+    for param in pdq:
+        for param_seasonal in seasonal_pdq:
+            try:
+                mod = sm.tsa.statespace.SARIMAX(y,
+                                            order=param,
+                                            seasonal_order=param_seasonal,
+                                            enforce_stationarity=False,
+                                            enforce_invertibility=False)
+ 
+                results = mod.fit()
+ 
+                print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
+            except:
+                continue
+    mod = sm.tsa.statespace.SARIMAX(y,
+                                order=(1, 1, 1),
+                                seasonal_order=(1, 1, 1, 12),
+                                enforce_stationarity=False,
+                                enforce_invertibility=False)
+ 
+    results = mod.fit()
+    print(results.summary().tables[1])
+ 
+    results.plot_diagnostics(figsize=(15, 12))
+    plt.show()
+
+    pred = results.get_prediction(start=pd.to_datetime('1998-01-01'), dynamic=False)
+    pred_ci = pred.conf_int()
+
+
+if __name__ == '__main__':
+    stationarity_Test()
