0088917a3694a3eb88025981c976cce8c913f4c5
星宇 郭 <gxydewr@hotmail.com>
2019-03-16 14:40:07 +0800

    修改

--- Tourist Predict--A case of Shanghai/LSTM.py
+++ Tourist Predict--A case of Shanghai/LSTM.py
@@ -14,6 +14,11 @@ from sklearn.preprocessing import MinMaxScaler
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
+plt.rcParams['savefig.dpi'] = 300 #图片像素
+plt.rcParams['figure.dpi'] = 300 #分辨率
+
 #对数据进行格式处理，匹配模型数据结构要求
 def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
 	"""
@@ -53,8 +58,8 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
 def lstm_Model(data):
 
     #取2013-2017年数据为训练数据，2018年数据为预测数据
-    x_train,x_test = data.ix[:60,:-1],data.ix[:,:-1],
-    y_train,y_test = data.ix[:60,-1],data.ix[:,-1]
+    x_train,x_test = data.ix[:60,:-1],data.ix[60:,:-1],
+    y_train,y_test = data.ix[:60,-1],data.ix[60:,-1]
 
 
     # reshape input to be 3D [samples, timesteps, features]
@@ -74,38 +79,32 @@ def lstm_Model(data):
     model.compile(loss='mse', optimizer='adam')
 
 
-    history = model.fit(x_train, y_train , batch_size=1,epochs=5, validation_data=(x_test, y_test), verbose=2, shuffle=False)
+    history = model.fit(x_train, y_train , batch_size=1,epochs=10, validation_data=(x_test, y_test), verbose=2, shuffle=False)
 
     predValue = model.predict(x_test)
 
     #对预测值于真实值的R2
     r2 = r2_score(np.array(y_test),np.array(predValue))
     print(r2)
-    '''
+
+    plt.title('上海旅游预测-LSTM r2={0}'.format(r2))
     plt.plot(np.array(y_test),color = 'red',label = 'tourist_Number')
     plt.plot(np.array(predValue),color = 'green',label = 'predValue')
     plt.legend()
     plt.show()
-    '''
+
 
     return predValue
 
 if __name__ == '__main__':
     idx = pd.period_range('1/1/2013','12/31/2017 ',freq='M')#时间索引
 
-    o_data = pd.read_excel(r'Data/建模数据.xlsx',sheet_name='优化模型',usecols=[0,1,2,3])
+    origin_data = pd.read_excel(r'Data/建模数据.xlsx',sheet_name='优化模型',usecols=[0,1,2,3])
 
-    ss = StandardScaler()
-
-    x_std = ss.fit_transform(o_data)#归一化
+    x_std = StandardScaler().fit_transform(origin_data)#归一化
 
     data = series_to_supervised(x_std)
-
-    tN_std = ss.fit_transform(pd.read_excel(r'Data/建模数据.xlsx',sheet_name='优化模型',usecols=[3]))#旅游人数归一化
-    pV = ss.inverse_transform(lstm_Model(data))#旅游人数还原
-    print(pV)
-
-    plt.plot(np.array(pd.read_excel(r'Data/建模数据.xlsx',sheet_name= '优化模型',usecols=[3])),color = 'red',label = 'tourist_Number')
-    plt.plot(pV,color = 'green',label = 'predValue')
-    plt.legend()
-    plt.show()
+    lstm_Model(data)
+    #tN_std = StandardScaler().fit_transform(pd.read_excel(r'Data/建模数据.xlsx',sheet_name='优化模型',usecols=[3]))#旅游人数归一化
+    #pV = StandardScaler().inverse_transform(lstm_Model(data))#旅游人数还原
+    #print(pV)
--- Tourist Predict--A case of Shanghai/Xgboost.py
+++ Tourist Predict--A case of Shanghai/Xgboost.py
@@ -7,6 +7,7 @@ from xgboost import plot_importance
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import ShuffleSplit
+from sklearn.preprocessing import StandardScaler
 
 plt.rcParams['font.sans-serif']=['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
@@ -15,42 +16,60 @@ plt.rcParams['figure.dpi'] = 300 #分辨率
 
 def trainandTest(X_train, Y_train, X_test,Y_test):
 
-
-    #model = xgb.XGBRegressor(max_depth=5, learning_rate=0.3, n_estimators=100, silent=True, objective='reg:gamma')
-    model = xgb.XGBClassifier(
-                 learning_rate =0.1,
-                 n_estimators=3000,
-                 max_depth=4,
-                 min_child_weight=1,
-                 gamma=0,
-                 subsample=0.8,
-                 colsample_bytree=0.8,
-                 objective= 'binary:logistic',
-                 nthread=4,
-                 scale_pos_weight=1,
-                 seed=27)
+    model = xgb.XGBRegressor(learning_rate=0.01, n_estimators=8500, max_depth=5, min_child_weight=5, seed=0,
+                             subsample=1, colsample_bytree=1, gamma=0, reg_alpha=0, reg_lambda=0)
     model.fit(X_train, Y_train)
     ans = model.predict(X_test)
-
     r2 = r2_score(np.array(Y_test),np.array(ans))
-    print(r2)
-
-
-    plt.plot(np.array(Y_test),color = 'red',label = 'tourist_Number')
-    plt.plot(np.array(ans),color = 'green',label = 'predValue')
+    print(ans)
+   
+    plt.title('上海旅游预测-xgboost r2={0}'.format(r2))
+    plt.plot(np.array(Y_test),color = 'red',label = 'tourist_Number',linewidth=4)
+    plt.plot(np.array(ans),color = 'green',label = 'predValue',linestyle="--")
     plt.legend()
+    plt.savefig(r'Pictures/xgboost-SHLY_origin_Data.png')
     plt.show()
+    '''
+    cv_params = {'colsample_bytree': [0.1,0.2,0.3,0.4,0.5,0.6,0.7]}
+    other_params = {'learning_rate': 0.1, 'n_estimators': 360, 'max_depth': 0, 'min_child_weight': 0, 'seed': 0,
+                    'subsample': 0.2, 'colsample_bytree': 0.1, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 0}
+
+    model = xgb.XGBRegressor(**other_params)
+
+    optimized_GBM = GridSearchCV(estimator=model, param_grid=cv_params, scoring='r2', cv=5, verbose=1, n_jobs=4)
+    optimized_GBM.fit(X_train, Y_train)
+    evalute_result = optimized_GBM.grid_scores_
+    print('每轮迭代运行结果:{0}'.format(evalute_result))
+    print('参数的最佳取值：{0}'.format(optimized_GBM.best_params_))
+    print('最佳模型得分:{0}'.format(optimized_GBM.best_score_))
+'''
+
+
+'''
+    最优参数
+    BaiduIndex
+    model = xgb.XGBRegressor(learning_rate=0.2, n_estimators=675, max_depth=5, min_child_weight=5, seed=1,
+                             subsample=0.8, colsample_bytree=0.7, gamma=0, reg_alpha=0, reg_lambda=1)
+    SHLY   
+    model = xgb.XGBRegressor(learning_rate=0.2, n_estimators=900, max_depth=5, min_child_weight=5, seed=0,
+                             subsample=0.6, colsample_bytree=0.5, gamma=0, reg_alpha=0, reg_lambda=0)
+    SHLY_origin_Data
+    
+    model = xgb.XGBRegressor(learning_rate=0.2, n_estimators=8500, max_depth=5, min_child_weight=5, seed=0,
+                             subsample=0.2, colsample_bytree=0.1, gamma=0, reg_alpha=0, reg_lambda=0)
+'''
+    
+    
+
 
 
 
 
 
 if __name__ == '__main__':
-    origin_Data = pd.read_csv(r'Data/origin_Data.csv',encoding='ISO-8859-1',usecols = [1,2,3,4])
-    X_train,X_test = np.array(origin_Data.ix[:60,:-1]),np.array(origin_Data.ix[:,:-1])
-    Y_train,Y_test = np.array(origin_Data.ix[:60,-1]),np.array(origin_Data.ix[:,-1])
-
-    #X_train, X_test, Y_train, Y_test = train_test_split(o_xList, o_yList, test_size=0.3, random_state=0)
+    origin_Data = pd.read_csv(r'Data/SHLY_origin_Data.csv',encoding='ISO-8859-1')
+    data_std =StandardScaler().fit_transform(origin_Data)#归一化
+    X_train,X_test = np.array(origin_Data)[:84,:-1],np.array(origin_Data)[84:,:-1]
+    Y_train,Y_test = np.array(origin_Data)[:84,-1],np.array(origin_Data)[84:,-1]
 
     trainandTest(X_train, Y_train, X_test,Y_test)
-
